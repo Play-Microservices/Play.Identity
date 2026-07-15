2097486fc75b8c343777f8bd9e80d9cc2715ff7e
Furmi <16333282+Furrman@users.noreply.github.com>
2026-07-15 12:03:49 +0100

    Add support to http requests

--- src/Play.Identity.API/Startup.cs
+++ src/Play.Identity.API/Startup.cs
@@ -1,7 +1,10 @@
 using System;
+using System.IO;
+using System.Reflection;
 using GreenPipes;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -50,6 +53,7 @@ public class Startup(IConfiguration configuration)
                 options.Events.RaiseSuccessEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseErrorEvents = true;
+                options.KeyManagement.KeyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!;
             })
             .AddAspNetIdentity<ApplicationUser>()
             .AddInMemoryApiScopes(identityServerSettings.ApiScopes)
@@ -92,6 +96,11 @@ public class Startup(IConfiguration configuration)
         app.UseIdentityServer();
 
         app.UseAuthorization();
+
+        app.UseCookiePolicy(new CookiePolicyOptions
+        {
+            MinimumSameSitePolicy = SameSiteMode.Lax
+        });
 
         app.UseEndpoints(endpoints =>
         {
